ab1c5183fdf249178049ef2627168db6825ea347
serigney <ssgueye1@cps.edu>
2025-03-20 16:24:22 +0000

    changes

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let size = document.querySelector(".size");
 let personality = document.querySelector(".personality");
 let button = document.querySelector("button");
 let answer = document.querySelector(".answer")
+let logo = document.querySelector(".logo")
+let Ant = document.querySelector(".Ant")
+logo.style.display  = "block";
+Ant.style.display  = "none";
 button.onclick = function() {
     console.log("button check");
     size = size.value
@@ -12,12 +16,17 @@ button.onclick = function() {
 if(size === "small" && personality === "aggressive"){
 console.log("Ant");
 answer.innerHTML = "You like players smaller players (Guards) that are aggressive and love to talk trash, like Anthony Edwards and Russell Westbrook ";
+logo.style.display  = "none";
+Ant.style.display  = "block";
 } else if (size === "small" && personality === "calm") {
     answer.innerHTML = "You like smaller players (Guards) calm and let their game talk for them like Curry, SGA, and Kyrie ";
+    logo.style.display  = "none";
 } else if (size === "big" && personality === "calm") {
     answer.innerHTML = "You like the bigs (Centers, Fowards) calm and let their game talk for them like Jokic, Giannis, Tim Duncan";
+    logo.style.display  = "none";
 }else if (size === "big" && personality === "aggressive") {
     answer.innerHTML = "You like the bigs (Centers, Fowards) that are aggressive and love to talk trash like Kevin Garnett, Larry Bird, KD";
+    logo.style.display  = "none";
 } else {
     answer.innerHTML = "Either your size or personality was wrong. Remember choose between small or big for size. For Personality choose between calm or aggressive. (Make sure to leave no spaces)"
 }
